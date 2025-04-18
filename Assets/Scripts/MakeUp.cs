714a0cd612cf03b1922fba04d99249f75d1ec217
안영진 <anyoungjin0106@kbu.ac.kr>
2025-04-18 16:28:57 +0900

    create app

---
+++ Assets/Scripts/MakeUp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeUp : MonoBehaviour
+{
+    MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh=GetComponent<MeshRenderer>();
+    }
+    public void SetColor(string hex){
+        Color color;
+        ColorUtility.TryParseHtmlString(hex,out color);
+        print(color);
+        mesh.material.color=color;
+    }
+}
