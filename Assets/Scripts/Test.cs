4f13dad8e51f4b1422ff68e51c4cfc1647081ddf
안영진 <anyoungjin0106@gmail.com>
2025-10-24 00:39:33 +0900

    드디어 이메일 연동 기능 추가
    이메일로 이제 비밀번호 찾기와 환영인사를 받을수 있게됌

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine.Android;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
@@ -9,14 +10,22 @@ using UnityEngine.UI;
 public class Test : MonoBehaviour
 {
     Button btn;
-    [SerializeField]Text msg;
+    [SerializeField] Text msg;
     bool canClick;
     void Start()
     {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+
+
         canClick = true;
         btn = GetComponent<Button>();
-        btn.onClick.AddListener(()=>{
-            if(canClick)
+        btn.onClick.AddListener(() =>
+        {
+            if (canClick)
                 StartCoroutine(Capture());
         });
     }
@@ -24,49 +33,59 @@ public class Test : MonoBehaviour
     IEnumerator Capture()
     {
         canClick = false;
-        SetBtn=false;//Hide the button
+        SetBtn = false;//Hide the button
         yield return new WaitForEndOfFrame();
         var img = ScreenCapture.CaptureScreenshotAsTexture();
         StartCoroutine(Post(img.EncodeToJPG()));
-        SetBtn=true;//Show the button
+        SetBtn = true;//Show the button
     }
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             SceneManager.LoadScene(0);
         }
     }
     IEnumerator Post(byte[] img)
     {
         WWWForm form = new WWWForm();
-        if(!Session.session.isGeust)
-            form.AddField("user_id", Session.session.UserId);
+        if (!Session.session.isGeust)
+            form.AddField("token", Session.session.Token);
         form.AddBinaryData("img", img);
         using (UnityWebRequest www = UnityWebRequest.Post(Env.Api("predict"), form))
         {
             yield return www.SendWebRequest();
             if (www.result == UnityWebRequest.Result.Success)
             {
-                ColorJson colorJson=JsonUtility.FromJson<ColorJson>(www.downloadHandler.text);
-                print(colorJson.color_id);
-                if(colorJson.description!=""){
+                ColorJson colorJson = JsonUtility.FromJson<ColorJson>(www.downloadHandler.text);
+                if (string.IsNullOrEmpty(colorJson.description))
+                {
+                    print("에러");
+                    msg.text = colorJson.color_id;
+                    Text text = btn.transform.GetChild(0).GetComponent<Text>();
+                    text.text = "퍼스널컬러 구하기";
+                    ColorBlock color = btn.colors;
+                    color.normalColor = new Color(1, 1, 1, 1);
+                    btn.colors = color;
+                    canClick = true;
+                }
+                else
+                {
+                    print("통과");
                     Session.session.Predict(colorJson);
                     SceneManager.LoadScene((int)Scene.Result);
-                }else{
-                    msg.text=colorJson.color_id;
-                    SetBtn=true;
-                    yield return new WaitForSeconds(2);
-                    msg.text="";
                 }
             }
         }
     }
-    bool SetBtn{
-        set{
-            msg.text="";
+    bool SetBtn
+    {
+        set
+        {
+            msg.text = "";
             Text text = btn.transform.GetChild(0).GetComponent<Text>();
             ColorBlock color = btn.colors;
             color.normalColor = new Color(1, 1, 1, value ? 1 : 0);
