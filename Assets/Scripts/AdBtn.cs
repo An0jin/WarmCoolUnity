3829b336fc56d56ccf76a6f04a33cc96d6b57a48
안영진 <anyoungjin0106@gmail.com>
2025-11-11 18:45:20 +0900

    광고오류

--- Assets/Scripts/AdBtn.cs
+++ Assets/Scripts/AdBtn.cs
@@ -4,15 +4,16 @@ using GoogleMobileAds.Api;
 
 public class AdBtn : MonoBehaviour
 {
-    [SerializeField] Button btn;
+    Button btn;
+    [SerializeField] Text msg;
     private InterstitialAd interstitialAd;
 
     void Start()
     {
-        if (btn == null) btn = GetComponent<Button>();
+        btn = GetComponent<Button>();
 
         MobileAds.Initialize(_ => { Debug.Log("AdMob Initialized"); });
-        // btn.interactable = false;
+        btn.interactable = false;
         LoadInterstitial();
 
         btn.onClick.AddListener(ShowInterstitial);
@@ -26,12 +27,14 @@ public class AdBtn : MonoBehaviour
             interstitialAd = null;
         }
 
-        InterstitialAd.Load(Env.adMobId, new AdRequest(), (ad, loadError) =>
+        InterstitialAd.Load(Env.interstitialId, new AdRequest(), (ad, loadError) =>
         {
             if (loadError != null)
             {
                 Debug.LogError($"[AD] 로드 실패: {loadError}");
-                // btn.interactable = false;
+                btn.interactable = false;
+                msg.color = new Color(1, 0, 0);
+                msg.text = $"[AD] 로드 실패: {loadError}";
                 // 실패 시 일정 시간 뒤 재시도하거나, 사용자 액션 때 재시도
                 return;
             }
@@ -39,7 +42,7 @@ public class AdBtn : MonoBehaviour
             interstitialAd = ad;
             RegisterCallbacks(interstitialAd);
             Debug.Log("[AD] 로드 성공");
-            // btn.interactable = true; // 준비되면 버튼 활성화
+            btn.interactable = true; // 준비되면 버튼 활성화
         });
     }
 
@@ -55,7 +58,9 @@ public class AdBtn : MonoBehaviour
         else
         {
             Debug.Log("[AD] 아직 준비 안 됨 → 재로드 시도");
-            // btn.interactable = false;
+            btn.interactable = false;
+            msg.color = new Color(1, 0, 0);
+            msg.text = "[AD] 아직 준비 안 됨 → 재로드 시도";
             LoadInterstitial();
         }
     }
@@ -72,7 +77,9 @@ public class AdBtn : MonoBehaviour
             Debug.Log("[AD] 닫힘 → 폐기 후 재로드");
             ad.Destroy();
             interstitialAd = null;
-            // btn.interactable = false;
+            btn.interactable = false;
+            msg.color = new Color(1, 0, 0);
+            msg.text = "[AD] 닫힘 → 폐기 후 재로드";
             LoadInterstitial(); // 다음 노출을 위해 즉시 새로 로드
         };
 
@@ -81,7 +88,9 @@ public class AdBtn : MonoBehaviour
             Debug.LogError($"[AD] 전체화면 열기 실패: {err} → 폐기 후 재로드");
             ad.Destroy();
             interstitialAd = null;
-            // btn.interactable = false;
+            btn.interactable = false;
+            msg.color = new Color(1, 0, 0);
+            msg.text = $"[AD] 전체화면 열기 실패: {err} → 폐기 후 재로드";
             LoadInterstitial();
         };
 
