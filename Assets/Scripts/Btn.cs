e4c41b7e692d6ce92e86d9f7d21639bec7a0d38b
안영진 <anyoungjin0106@gmail.com>
2025-12-15 03:23:13 +0900

    Update Btn.cs

--- Assets/Scripts/Btn.cs
+++ Assets/Scripts/Btn.cs
@@ -5,13 +5,10 @@ public abstract class Btn : MonoBehaviour
 {
     protected Button btn;
 
-    // 2. Awake 권장: Start보다 먼저 실행되어 초기화 순서에서 안전함
     protected virtual void Awake()
     {
         btn = GetComponent<Button>();
         btn.onClick.AddListener(OnClick);
     }
-
-    // 3. abstract method: "자식들아, 내용은 너희가 무조건 채워라. 안 채우면 에러 낼 거야."
     protected abstract void OnClick();
 }
