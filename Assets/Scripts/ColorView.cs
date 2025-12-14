5272ebfa5a0d78c1d53b119feb53340eb638d3c8
안영진 <anyoungjin0106@gmail.com>
2025-12-15 04:07:16 +0900

    Update ColorView.cs

--- Assets/Scripts/ColorView.cs
+++ Assets/Scripts/ColorView.cs
@@ -8,24 +8,15 @@ public class ColorView : MonoBehaviour
 {
     void Start()
     {
-        StartCoroutine(SetColorView());
-    }
 
-    IEnumerator SetColorView()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get(Env.Api($"/lipstick/{Session.session.ColorId}")))
+        StartCoroutine(APIManager.Get($"/lipstick/{Session.session.ColorId}", (jsonText) =>
         {
-            yield return www.SendWebRequest();
-            if (www.result == UnityWebRequest.Result.Success)
+            JsonList<ColorJson> json = JsonUtility.FromJson<JsonList<ColorJson>>(jsonText);
+            foreach (var item in json.result)
             {
-                print(www.downloadHandler.text);
-                JsonList<ColorJson> json = JsonUtility.FromJson<JsonList<ColorJson>>(www.downloadHandler.text);
-                foreach (var item in json.result)
-                {
-                    ColorBtn btn = Instantiate(Resources.Load<ColorBtn>("ColorBtn"), transform);
-                    btn.SetBtnColor(item.hex_code, item.cname);
-                }
+                ColorBtn btn = Instantiate(Resources.Load<ColorBtn>("ColorBtn"), transform);
+                btn.SetBtnColor(item.hex_code, item.cname);
             }
-        }
+        }));
     }
 }
